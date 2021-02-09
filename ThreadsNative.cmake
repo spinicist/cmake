0a2997d918af21e3eec46bfd5ebafa9ae03030da
Tobias Wood <tobias@mi3.com>
2021-02-09 14:04:30 +0000

    Add Thread Sanitizer toolchain file

---
+++ ThreadsNative.cmake
@@ -0,0 +1,17 @@
+set(MY_FLAGS "-march=native -Wall -Wpedantic -Wshadow")
+set(MY_FLAGS_DEBUG "-fsanitize=thread -O2")
+set(CMAKE_C_FLAGS_INIT ${MY_FLAGS})
+set(CMAKE_CXX_FLAGS_INIT ${MY_FLAGS})
+set(CMAKE_C_FLAGS_DEBUG_INIT ${MY_FLAGS_DEBUG})
+set(CMAKE_CXX_FLAGS_DEBUG_INIT ${MY_FLAGS_DEBUG})
+
+include(CheckCXXCompilerFlag)
+include(CMakeToolsHelpers OPTIONAL)
+include(CMakePrintHelpers)
+include(${CMAKE_CURRENT_LIST_DIR}/BuildType.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/vcpkg/scripts/buildsystems/vcpkg.cmake)
+
+find_program(CCACHE_PROGRAM ccache)
+if(CCACHE_PROGRAM)
+    set_property(GLOBAL PROPERTY RULE_LAUNCH_COMPILE "${CCACHE_PROGRAM}")
+endif()
