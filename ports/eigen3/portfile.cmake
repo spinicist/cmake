bf038759e5ea24ec24732ec0e0afa7090ca8b381
Tobias Wood <tobias@spinicist.org.uk>
2023-04-21 09:33:43 +0100

    Add a port for Eigen master branch

--- ports/eigen3/portfile.cmake
+++ ports/eigen3/portfile.cmake
@@ -4,18 +4,15 @@ vcpkg_from_gitlab(
     GITLAB_URL https://gitlab.com
     OUT_SOURCE_PATH SOURCE_PATH
     REPO libeigen/eigen
-    REF 9b51dc7972c9f64727e9c8e8db0c60aaf9aae532
-    SHA512 2f5c2160ac8492dbff8e827104aa52f72d3abab56489c8281e92bb737603028861541690962f7004aedf9047674896634aaa03489d6d5e14fe2dd9864a0b95e6
+    REF b0eded878d5d162d61583a286c0d8a45406ad1bc
+    SHA512 effa431e2decd3ad5d82c83bc8adcb4f346dc0206a2b0d2ebf2e0e4aaa721697df652957b3aec99100591e35fc8dca587ebb79a1ce0b54d546b01dbc0c5a1cba
     HEAD_REF master
     PATCHES
-        remove_relative_path_check.patch
-        # disable_pkgconfig_absolute_path_check.patch
-        # fix-cuda-error.patch # issue https://gitlab.com/libeigen/eigen/-/issues/1526
+        remove_configure_checks.patch # This removes unnecessary configure checks. Eigen3 just installs headers not anything more.
 )
 
-vcpkg_configure_cmake(
-    SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
         -DBUILD_TESTING=OFF
         -DEIGEN_BUILD_PKGCONFIG=ON
@@ -27,13 +24,13 @@ vcpkg_configure_cmake(
         -DPKGCONFIG_INSTALL_DIR=${CURRENT_PACKAGES_DIR}/debug/lib/pkgconfig
 )
 
-vcpkg_install_cmake()
-vcpkg_fixup_cmake_targets()
+vcpkg_cmake_install()
+vcpkg_cmake_config_fixup()
 vcpkg_fixup_pkgconfig()
 
-file(GLOB INCLUDES ${CURRENT_PACKAGES_DIR}/include/eigen3/*)
+file(GLOB INCLUDES "${CURRENT_PACKAGES_DIR}/include/eigen3/*")
 # Copy the eigen header files to conventional location for user-wide MSBuild integration
-file(COPY ${INCLUDES} DESTINATION ${CURRENT_PACKAGES_DIR}/include)
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include ${CURRENT_PACKAGES_DIR}/debug/share)
+file(COPY ${INCLUDES} DESTINATION "${CURRENT_PACKAGES_DIR}/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include" "${CURRENT_PACKAGES_DIR}/debug/share")
 
-file(INSTALL ${SOURCE_PATH}/COPYING.README DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
+file(INSTALL "${SOURCE_PATH}/COPYING.README" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
