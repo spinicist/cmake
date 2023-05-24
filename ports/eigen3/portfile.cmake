e6decff6f6a738fc44da069c64546574531a596a
Tobias Wood <tobias.wood@kcl.ac.uk>
2023-05-24 16:27:22 +0100

    Update Eigen port

--- ports/eigen3/portfile.cmake
+++ ports/eigen3/portfile.cmake
@@ -1,5 +1,3 @@
-vcpkg_buildpath_length_warning(37)
-
 vcpkg_from_gitlab(
     GITLAB_URL https://gitlab.com
     OUT_SOURCE_PATH SOURCE_PATH
@@ -7,30 +5,33 @@ vcpkg_from_gitlab(
     REF b0eded878d5d162d61583a286c0d8a45406ad1bc
     SHA512 effa431e2decd3ad5d82c83bc8adcb4f346dc0206a2b0d2ebf2e0e4aaa721697df652957b3aec99100591e35fc8dca587ebb79a1ce0b54d546b01dbc0c5a1cba
     HEAD_REF master
-    PATCHES
-        remove_configure_checks.patch # This removes unnecessary configure checks. Eigen3 just installs headers not anything more.
 )
 
-vcpkg_cmake_configure(
-    SOURCE_PATH "${SOURCE_PATH}"
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}
+    PREFER_NINJA
     OPTIONS
         -DBUILD_TESTING=OFF
         -DEIGEN_BUILD_PKGCONFIG=ON
     OPTIONS_RELEASE
-        -DCMAKEPACKAGE_INSTALL_DIR=${CURRENT_PACKAGES_DIR}/share/eigen3
-        -DPKGCONFIG_INSTALL_DIR=${CURRENT_PACKAGES_DIR}/lib/pkgconfig
+        "-DCMAKEPACKAGE_INSTALL_DIR:PATH=share/eigen3"
+        "-DPKGCONFIG_INSTALL_DIR:PATH=lib/pkgconfig"
     OPTIONS_DEBUG
-        -DCMAKEPACKAGE_INSTALL_DIR=${CURRENT_PACKAGES_DIR}/debug/share/eigen3
-        -DPKGCONFIG_INSTALL_DIR=${CURRENT_PACKAGES_DIR}/debug/lib/pkgconfig
+        "-DCMAKEPACKAGE_INSTALL_DIR:PATH=debug/share/eigen3"
+        "-DPKGCONFIG_INSTALL_DIR:PATH=lib/pkgconfig"
+)
+vcpkg_install_cmake()
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include" "${CURRENT_PACKAGES_DIR}/debug/share")
+
+vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/share/eigen3/Eigen3Targets.cmake"
+    "set(_IMPORT_PREFIX " "get_filename_component(_IMPORT_PREFIX \"\${CMAKE_CURRENT_LIST_DIR}/../..\" ABSOLUTE) #"
 )
 
-vcpkg_cmake_install()
-vcpkg_cmake_config_fixup()
 vcpkg_fixup_pkgconfig()
 
 file(GLOB INCLUDES "${CURRENT_PACKAGES_DIR}/include/eigen3/*")
-# Copy the eigen header files to conventional location for user-wide MSBuild integration
 file(COPY ${INCLUDES} DESTINATION "${CURRENT_PACKAGES_DIR}/include")
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include" "${CURRENT_PACKAGES_DIR}/debug/share")
 
 file(INSTALL "${SOURCE_PATH}/COPYING.README" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+configure_file("${CURRENT_PORT_DIR}/vcpkg-cmake-wrapper.cmake" "${CURRENT_PACKAGES_DIR}/share/${PORT}/vcpkg-cmake-wrapper.cmake" COPYONLY)
