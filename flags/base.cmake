7555cbafbcd953f67a5280ecf582c08a7917dc35
Tobias Wood <tobias@spinicist.org.uk>
2023-10-03 21:16:05 +0100

    Remove M1 flags file

--- flags/base.cmake
+++ flags/base.cmake
@@ -1,2 +1,2 @@
-set(MY_FLAGS "-Wall -Wpedantic -Wshadow -Wconversion")
+set(MY_FLAGS "-Wall -Wpedantic -Wshadow")
 set(MY_FLAGS_DEBUG "-fsanitize=address,undefined")
--- flags/m1.cmake
+++
@@ -1,2 +0,0 @@
-set(MY_FLAGS "-mcpu=apple-m1 -Wall -Wpedantic -Wshadow")
-set(MY_FLAGS_DEBUG "-fsanitize=address,undefined")
