988152b7600f104636c5d09aeaec60df7b25b88b
Tobias Wood <tobias@spinicist.org.uk>
2024-02-07 11:33:31 +0000

    Bump Eigen

--- ports/eigen3/portfile.cmake
+++ ports/eigen3/portfile.cmake
@@ -7,12 +7,10 @@ vcpkg_from_gitlab(
     GITLAB_URL https://gitlab.com
     OUT_SOURCE_PATH SOURCE_PATH
     REPO libeigen/eigen
-    REF a798d076596343b89b18e44c8033d77d22e19892
-    SHA512 fa5b4566fefb8300922f7aaf309ab2a0cbe0266c77f3be8f06c76d9a8622ee0c99bc2f7673ac7b127d356986a53aceb8f56388c02a014f8d4d64b546c5d341cf
+    REF 431e4a913bcf7c37208361868b7e202a8a8f5e4d
+    SHA512 16c6760a91e8c10b3ec3de8eb84337c75363e78b5905d7af4ab19fa56c4e1995abe2a9ed8ffe0b213bdcb4dd3248132f308c65b4b418e2f62c5ebb3eb2d3265a
     HEAD_REF master
     PATCHES
-        remove_configure_checks.patch # This removes unnecessary configure checks. Eigen3 just installs headers not anything more.
-        fix-vectorized-reductions-half.patch # Remove this patch in the next update
 )
 
 # vcpkg_from_git(
